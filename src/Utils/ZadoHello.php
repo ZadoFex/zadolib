655e0f315f9c6d74f9fb700b43702c69d55ffb8a
Giacomo Festuccia <giacomo.festuccia@merendels.com>
2019-03-07 12:29:24 +0100

    add

--- src/Utils/ZadoHello.php
+++ src/Utils/ZadoHello.php
@@ -6,19 +6,7 @@ class ZadoHello
 {
     public static function hello()
     {
-        return 'Hello zado utils lib !\n';
-    }
-
-    public static function NewGuid()
-    {
-        $s = strtoupper(md5(uniqid(rand(),true))); 
-		$guidText = 
-			substr($s,0,8) . '-' . 
-			substr($s,8,4) . '-' . 
-			substr($s,12,4). '-' . 
-			substr($s,16,4). '-' . 
-			substr($s,20); 
-		return $guidText;
+        return 'Hello zado utils lib !';
     }
 }
 
